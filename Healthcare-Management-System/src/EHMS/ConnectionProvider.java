6a2dfeeac9e627efd10be75c8443aad5ae78abd9
Venuthurla Venkata Pradeep Reddy <51847492+pradeep9redddy@users.noreply.github.com>
2022-04-25 20:50:49 +0530

    Add files via upload

---
+++ Healthcare-Management-System/src/EHMS/ConnectionProvider.java
@@ -0,0 +1,20 @@
+package EHMS;
+
+import java.sql.*;
+
+public class ConnectionProvider {
+	public static Connection getCon() {
+		try {
+			String url = "jdbc:mysql://localhost:3306/HealthcareMangaementSystem";
+
+			String uname = "root";
+			String pass = "2000";
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection(url, uname, pass);
+			return con;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+}
